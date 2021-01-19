8d4657f96a51a6ea18f68f79ad22651ee5769322
María René Ruiz Flores <a2019117746@estudiantes.upsa.edu.bo>
2021-01-18 21:53:10 -0400

    Modelo persona creado

---
+++ apiPersona/WebapiPersona/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿
+namespace WebapiPersona.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public enum SexType
+    {
+        Femenino=0,
+        Masculino=1
+    }
+    public class Person
+    {
+        [Key]
+        public int Personid { get; set; } //Aquí escribimos prop e hicimos tap tap 
+        [Required]
+        [StringLength(80,ErrorMessage = "Favor ingrese su nombre entre 5 y 80 caracteres", MinimumLength = 5)]
+        public string Name { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Birthay { get; set; }
+        public SexType Sex { get; set; }
+
+
+    }
+}
