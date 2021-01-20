f8fddea52025fdcd3236784a819fa4e9791b16d8
María René Ruiz Flores <a2019117746@estudiantes.upsa.edu.bo>
2021-01-19 22:06:42 -0400

    Se creo el proyecto, el modelo, el controlador y se enlazo a la base de datos

---
+++ apiPersona/WebapiPersona/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+namespace WebapiPersona.Models
+{
+    using System.Data.Entity;
+    public class DataContext: DbContext //Aumentamos :DbContext y creamos la libreria para que ya no marque error. Click derecho en el proyecto y le damos a Administrar paquetes nuget, en el buscador ponemos entityframework, le damos examinar y esocogemos el primero e instalar y aceptar a todo. Le damos otra vez control punto a la palabra y escogemos el primero para crear la libreria y nos dejará de dar error
+    {
+        // ctor, tap tap y aparece public DataContext(){}
+        public DataContext():base("DefaultConnection") //escribimos :base("DefaultConnection") Con esto terminamos de enlazar con la base de datos wuuu 20 puntos
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebapiPersona.Models.Person> People { get; set; }
+    }
+}
